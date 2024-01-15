ee9d3653c86d6cbefc5b639a6ba3c6fc0fff928e
Skye McLean <skyemclean87@gmail.com>
2024-01-14 18:13:50 -0800

    Added About page container and flex display

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
+    <div className='about-container'>
       <div className='about-title'>Our Mission</div>
       <div className='about'>
-        Twitch Notes was developed to be an organization
+        Twitch Notes was developed to be an organizational
         tool for busy live streamers. It is built to aid
         broadcasters in managing various off and on stream
         tasks with full integration to Twitch allowing for
-        ease of use and intuitive implementation.
+        ease of use and intuitive full implementation with 
+        Twitch API.
       </div>
       <a href="/tasktracker"><button className='notes-btn'>GET STARTED</button></a>
     </div>
